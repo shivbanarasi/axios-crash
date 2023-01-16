f9104ebf31ed2943194cbec4f76f7b517beec80a
shivbanarasi <113574281+shivbanarasi@users.noreply.github.com>
2023-01-16 08:19:53 +0530

    axios code updated

--- main.js
+++ main.js
@@ -1,51 +1,133 @@
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  axios('https://jsonplaceholder.typicode.com/todos')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios('https://jsonplaceholder.typicode.com/todos',{
+    title:'New Todo',
+    completed:false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios('https://jsonplaceholder.typicode.com/todos/1',{
+    title:'updated Todo',
+    completed:false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err)); 
 }
-
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+ axios.all([
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+  axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+ ])
+ .then(axios.spread((todos,posts)=>showOutput(posts)))
+ .catch(err=>console.error(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config={
+    headers:{
+      'constent-Type':'application/json',
+      Authorization:'sometoken'
+    }
+  }
+  axios('https://jsonplaceholder.typicode.com/todos',{
+    title:'New Todo',
+    completed:false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err))
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options={
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    data:{
+      title:'Hello World'
+    },
+    transformResponse:axios.defaults.transformResponse.concat(data=>{
+      data.title=data.title.toUpperCase();
+      return data;
+    })
+  }
+  axios(options).then(res=>showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios('https://jsonplaceholder.typicode.com/todos')
+  .then(res=>showOutput(res))
+  .catch(err=>{ if(error.response){
+    console.log(err.response.data);
+    console.log(err.response.status);
+    console.log(err.response.headers);
+    if(err.response.status===404){
+      alert("error:page not found");
+    }
+    
+  }
+  else if(err.request){
+    console.error(err.request);
+  }else{
+    console.error(err.massage);
+  }
+});
+ 
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source=axios.CancelToken.source();
+  axios('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken:source.token
+  })
+
+  .then(res=>showOutput(res))
+  .catch(thrown=>{
+    if(axios.isCancel(thrown)){
+      console.log('request canceled',thrown.massage);
+    }
+  });
+  if(true){
+    source.cancel('Request canceled')
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(config=>{
+  console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
+  return config;
+},
+err=>{
+  return Promise.reject(error);
+}
+);
 
 // AXIOS INSTANCES
+const axiosInstance=axios.create({
+  baseURL:"https://jsonplaceholder.typicode.com"
+})
+axiosInstance.get('/comments').then(res=>showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
@@ -53,7 +135,6 @@ function showOutput(res) {
   <div class="card card-body mb-4">
     <h5>Status: ${res.status}</h5>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Headers
@@ -62,7 +143,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.headers, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Data
@@ -71,7 +151,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.data, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Config
